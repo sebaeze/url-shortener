7ea9acb73ecefe97a5dcf8d15a4e9d6fc965a97b
sebastian_andreoletti <andreole@ar.ibm.com>
2024-10-26 20:21:42 -0300

    inicial

---
+++ backend/main.js
@@ -0,0 +1,18 @@
+/*
+*
+*/
+function main(args){
+    //
+    return {
+        headers: { "Content-Type": "application/json;charset=utf-8" },
+        // use the text generator to create a response sentence 
+        // with 10 words 
+        body: {
+            "mensaje": "_noseee_"
+        },
+      };
+    //
+}
+//
+module.exports.main = main;
+//
